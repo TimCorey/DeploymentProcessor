46c05362eaa467abd8dfbfb83b71d28c526c87ea
Parrish Husband <parrish.husband@wlr-llc.com>
2018-05-22 13:36:18 -0500

    Added configuration methods and separated into own class. Updated DataAccess to be database agnostic.

--- ProcessorLibrary/DataAccess.cs
+++ ProcessorLibrary/DataAccess.cs
@@ -1,44 +1,87 @@
 ﻿using Dapper;
 using System;
-using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Configuration;
 using System.Data;
-using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
+using System.Data.Common;
 using System.Threading.Tasks;
 
 namespace ProcessorLibrary
 {
     public static class DataAccess
     {
-        private static string LoadConnectionString(string id = "FitIndex")
+        private static readonly string LogDatabase = "LoggingDb";
+        private static readonly ConcurrentDictionary<string, DbProviderFactory> ProviderFactories = new ConcurrentDictionary<string, DbProviderFactory>();
+
+        public static int InsertDeploymentAction(string deploymentType, string siteName)
         {
-            return System.Configuration.ConfigurationManager.ConnectionStrings[id].ConnectionString;
+            var param = new
+            {
+                DeploymentType = deploymentType,
+                LoggedInUser = string.Empty,
+                SiteName = siteName
+            };
+
+            return RunProcedure(LogDatabase, "dbo.spDeploymentLog_InsertAction", param);
         }
 
-        public static void InsertOutOfCycleFileChange(string fileName, string changeType)
+        public static int InsertOutOfCycleFileChange(string fileName, string changeType)
         {
-            using (IDbConnection cnn = new SqlConnection(LoadConnectionString("LoggingDb")))
+            var param = new
             {
-                var p = new DynamicParameters();
-                p.Add("@FileChanged", fileName);
-                p.Add("@ChangeType", changeType);
+                FileChange = fileName,
+                ChangeType = changeType
+            };
 
-                cnn.Execute("dbo.spOutOfCycleFileChanges_Insert", p, commandType: CommandType.StoredProcedure);
+            return RunProcedure(LogDatabase, "dbo.spOutOfCycleFileChanges_Insert", param);
+        }
+
+        private static int RunProcedure(string connectionKey, string procedureName, object param = null)
+        {
+            using (IDbConnection connection = GetConnection(connectionKey))
+            {
+                return connection.Execute(procedureName, param, commandTimeout: connection.ConnectionTimeout, commandType: CommandType.StoredProcedure);
             }
         }
 
-        public static void InsertDeploymentAction(string deploymentType, string siteName)
+        private static Task<int> RunProcedureAsync(string connectionKey, string procedureName, object param = null)
         {
-            using (IDbConnection cnn = new SqlConnection(LoadConnectionString("LoggingDb")))
+            using (IDbConnection connection = GetConnection(connectionKey))
             {
-                var p = new DynamicParameters();
-                p.Add("@DeploymentType", deploymentType);
-                p.Add("@LoggedInUser", "");
-                p.Add("@SiteName", siteName);
-
-                cnn.Execute("dbo.spDeploymentLog_InsertAction", p, commandType: CommandType.StoredProcedure);
+                return connection.ExecuteAsync(procedureName, param, commandTimeout: connection.ConnectionTimeout, commandType: CommandType.StoredProcedure);
             }
+        }
+
+        private static IDbConnection GetConnection(string connectionName)
+        {
+            ConnectionStringSettings settings = Configuration.GetConnectionSettings(connectionName);
+            DbProviderFactory factory = GetProviderFactory(settings.ProviderName);
+
+            try
+            {
+                IDbConnection connection = factory.CreateConnection();
+                connection.ConnectionString = settings.ConnectionString;
+                return connection;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"{factory.GetType().Name} failed to create a connection.", ex);
+            }
+        }
+
+        private static DbProviderFactory GetProviderFactory(string providerName = "System.Data.SqlClient")
+        {
+            DbProviderFactory providerFactory;
+            if (!ProviderFactories.TryGetValue(providerName, out providerFactory))
+            {
+                providerFactory = DbProviderFactories.GetFactory(providerName);
+                if (providerFactory == null)
+                    throw new NotSupportedException($"{providerName} database provider is not available.");
+
+                ProviderFactories[providerName] = providerFactory;
+            }
+
+            return providerFactory;
         }
     }
 }
